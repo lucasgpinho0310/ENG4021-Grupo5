10baa30917f49b1145daeb8ae094bedcc3ba14d3
LucasP <lucas.g.pinho1999@gmail.com>
2026-08-28 12:04:17 +0000

    Calculadora completa com todas as operações. Falta adicionar o comando de escolher a operação

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -1,13 +1,21 @@
 def somar(a, b):
     return a + b
 
-def dividir(,a b):
+def dividir(a, b):
     if b == 0:
        Return "Erro: divisão por zero!"
     return a / b
 
 def subtrair(a, b):
     return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+def operacao():
+    print("=== Escolha a operação ===")
+    somar = 
 
 def calculadora():
     print("=== Calculadora Simples ===")
@@ -22,5 +30,8 @@ def calculadora():
 
     resultado_subtracao = subtrair(a, b)
     print(f"Resultado da subtração:{resultado_subtracao}")
+
+    resultado_multiplicacao = multiplicar(a, b)
+    print(f"Resultado da multiplicação:{resultado_multiplicacao}")
 if __name__ == "__main__":
     calculadora()
